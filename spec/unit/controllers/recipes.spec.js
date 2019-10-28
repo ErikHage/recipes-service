74bba59f38b0ca2b210db65b3e96d4ada41294ca
Erik Hage <ehage4@gmail.com>
2019-10-28 19:29:56 -0400

    Added logging, error factory, test coverage

---
+++ spec/unit/controllers/recipes.spec.js
@@ -0,0 +1,14 @@
+// const recipesController = require('../../../lib/controllers/recipes');
+// const recipesService = require('../../../lib/services/recipes');
+
+describe('Recipes Controller', () => {
+  describe('#getRecipes', () => {
+    it('should fetch the recipe names and ids');
+    it('should pass errors to next');
+  });
+
+  describe('#getRecipe', () => {
+    it('should fetch the recipe for the given id');
+    it('should pass errors to next');
+  });
+});
